81a5745cde113953d53f8e9779b485e3d04ad0f6
Steve Ou <me@oushaobin.cn>
2023-10-13 23:14:59 +0800

    Init project

---
+++ M3U8Fetcher.js
@@ -0,0 +1,58 @@
+var https = require('https');
+var m3u8Parser = require('m3u8-parser')
+
+class M3U8Client {
+
+    constructor(path){
+        this.path = path;
+    }
+    
+
+    fetch(callBack){
+        https.get(this.path, (res) => {
+            let m3u8Content = ''
+            res.on('data', (chunk)=> {m3u8Content += chunk})
+            res.on('end', ()=>{
+                let parser = new m3u8Parser.Parser();
+                parser.push(m3u8Content)
+                parser.end();
+                if(!parser.manifest){
+                    return;
+                }
+                let allChannel = []
+                parser.manifest.segments.forEach((entry=>{
+                    let title = entry.title;
+                    if(!title){
+                        return;
+                    }
+                    let titleSplit = title.split(' ')
+                    let titleMap = new Map();
+                    titleSplit.forEach(entry => {
+                        if(entry.indexOf('=') == -1){
+                            return;
+                        }
+                        let entryPart = entry.split('=');
+                        titleMap.set(entryPart[0], entryPart[1])
+                    })
+                    let titleName = titleMap.get('tvg-name')
+                    if(!titleName){
+                        titleName = titleMap.get('tvg-id')
+                    }
+                    if(!titleName){
+                        titleName = titleMap.get('group-title')
+                    }
+                    if(!titleName){
+                        titleName = '未知频道'
+                    }
+                    titleName = titleName.replace(/"/g,'')
+                    allChannel.push({title: titleName, uri: entry.uri})
+                }))
+                if(callBack){
+                    callBack(allChannel)
+                }
+            })
+        });
+    }
+}
+
+module.exports = M3U8Client
---
+++ index.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+
+var MediaRendererClient = require('upnp-mediarenderer-client');
+var UpnpSearcher = require('./UpnpSearcher')
+const inquirer = require("inquirer");
+const M3U8Client = require('./M3U8Fetcher')
+
+let allDLNARender = []
+let allChannel = []
+let selectedChannel = null;
+let selectedRemoter = null;
+
+function showMainMenu() {
+    console.clear();
+    if(!allChannel || allChannel.length == 0){
+        console.log('[频道数据] 搜索频道中')
+    }
+    if (selectedRemoter) {
+        console.log(`当前投屏设备: ${selectedRemoter.name}`)
+    }
+    if (selectedChannel) {
+        console.log(`当前频道: ${selectedChannel.title}`)
+    }
+    inquirer.prompt([
+        {
+            type: 'rawlist',
+            name: 'operation',
+            message: '亲，请选择操作~',
+            choices: ['选频道', '投屏'],
+        }]).then((answers) => {
+            if (answers.operation === '投屏') {
+                selectRemoteRender();
+            } else if (answers.operation === '选频道') {
+                selectChannel();
+            } else {
+                showMainMenu();
+            }
+        });
+}
+
+function selectRemoteRender() {
+    console.clear();
+    if (!allDLNARender || allDLNARender.length == 0) {
+        inquirer.prompt([{
+            type: 'confirm',
+            name: 'retry',
+            message: '搜索中... 还没找到可投屏设备, 重试一下吗?',
+            default: false,
+            transformer: (answer) => (answer ? '👍' : '👎'),
+        }]).then((answers) => {
+            if (answers.retry) {
+                selectRemoteRender();
+            } else {
+                showMainMenu()
+            }
+        })
+        return;
+    }
+    let choices = allDLNARender.map((value) => {
+        return {
+            name: value.name,
+            value
+        }
+    });
+    inquirer.prompt([{
+        type: 'rawlist',
+        name: 'selectedRemoter',
+        message: '请选择需要投屏的目标设备',
+        choices
+    }]).then((answers) => {
+        selectedRemoter = answers.selectedRemoter;
+        play();
+    })
+}
+
+function selectChannel() {
+    console.clear();
+    if (!allChannel || allChannel.length == 0) {
+        inquirer.prompt([{
+            type: 'confirm',
+            name: 'retry',
+            message: '搜索中... 还没拉取到频道数据, 重试一下吗?',
+            default: false,
+            transformer: (answer) => (answer ? '👍' : '👎'),
+        }]).then((answers) => {
+            if (answers.retry) {
+                selectChannel();
+            } else {
+                showMainMenu()
+            }
+        })
+        return;
+    }
+    let choices = allChannel.map((value) => {
+        return {
+            name: value.title,
+            value
+        }
+    });
+    inquirer.prompt([{
+        type: 'rawlist',
+        name: 'selectedChannel',
+        message: '亲要看哪个频道呢',
+        choices
+    }]).then((answers) => {
+        selectedChannel = answers.selectedChannel;
+        play();
+    })
+}
+
+function play(){
+    if (selectedRemoter && selectedChannel) {
+        let client = new MediaRendererClient(selectedRemoter.address);
+        client.load(selectedChannel.uri, {}, function (err, result) {
+            if (err) throw err;
+            console.log('正在投屏到 ...' + selectedChannel.title);
+            setTimeout(()=>{showMainMenu()}, 3000);
+        });
+        client.play();
+    }else{
+        showMainMenu();
+    }
+}
+
+let upnpSearcher = new UpnpSearcher()
+upnpSearcher.doSearch((deviceInfo) => {
+    allDLNARender.push(deviceInfo)
+})
+
+let m3U8Client = new M3U8Client('https://live.fanmingming.com/tv/m3u/global.m3u');
+m3U8Client.fetch((channel) => { allChannel = channel })
+
+showMainMenu();
+
+
